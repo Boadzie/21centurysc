b43a376f1e2c992d3753ab353e8c98a03a110183
Victor Bruce <victorbruce82@gmail.com>
2018-06-26 03:48:38 +0000

    Styled the discover page

--- src/components/discover.js
+++ src/components/discover.js
@@ -13,9 +13,25 @@ const Discover = () => {
                         </div>
                     </div>
                 </div>
+                <span></span>
             </section>
-            <section>
-
+            <section className="discover-form">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 offset-md-2">
+                            <form>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Example label</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">Another label</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </section>
             <Footer />
 
