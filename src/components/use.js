5c7cff414dd3d48029663d09a1cc03b029f71afc
Victor Bruce <victorbruce82@gmail.com>
2018-07-24 09:32:26 +0000

    Designed the use page

--- src/components/use.js
+++ src/components/use.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+// COMPONENTS
+import Footer from './Home/footer';
+
 
 const Use = () => {
-    return (<div>Use Page</div>);
+    return (
+        <div>
+            <section className="use">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h1 className="text-center">Use</h1>
+                        </div>
+                    </div>
+                </div>
+                <span></span>
+            </section>
+            <section className="clinics">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h2 className="card-title">Personal Clinic</h2>
+                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <Link to="" className="btn btn-primary float-right">More...</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h2 className="card-title">Personal Clinic</h2>
+                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <Link to="" className="btn btn-primary float-right">More...</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h2 className="card-title">Personal Clinic</h2>
+                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <Link to="" className="btn btn-primary float-right">More...</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h2 className="card-title">Personal Clinic</h2>
+                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <Link to="" className="btn btn-primary float-right">More...</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </div>
+    )
 }
 
 export default Use;
